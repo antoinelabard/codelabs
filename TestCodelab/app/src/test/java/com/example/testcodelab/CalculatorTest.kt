48259cc6639dba5191acc0cd8a8e693723c0b6ac
Antoine Labard <antoine@labard.fr>
2020-09-30 19:36:02 +0200

    Added division tests to TestCodelab.

--- TestCodelab/app/src/test/java/com/example/testcodelab/CalculatorTest.kt
+++ TestCodelab/app/src/test/java/com/example/testcodelab/CalculatorTest.kt
@@ -23,6 +23,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.lang.IllegalArgumentException
 import kotlin.math.pow
 
 
@@ -85,33 +86,41 @@ class CalculatorTest {
 
     @Test
     fun subTwoNumbers() {
-        val resultAdd = mCalculator.sub(1.0, 1.0)
-        assertThat(resultAdd, `is`(equalTo(0.0)))
+        val resultSub = mCalculator.sub(1.0, 1.0)
+        assertThat(resultSub, `is`(equalTo(0.0)))
     }
 
     @Test
     fun subTwoNumbersNegative() {
-        val resultAdd = mCalculator.sub(1.0, -1.0)
-        assertThat(resultAdd, `is`(equalTo(2.0)))
+        val resultSub = mCalculator.sub(1.0, -1.0)
+        assertThat(resultSub, `is`(equalTo(2.0)))
     }
 
     @Test
     fun subTwoNumbersInfinity() {
-        val resultAdd = mCalculator.sub(1.0, Double.POSITIVE_INFINITY)
-        assertThat(resultAdd, `is`(equalTo(Double.NEGATIVE_INFINITY)))
+        val resultSub = mCalculator.sub(1.0, Double.POSITIVE_INFINITY)
+        assertThat(resultSub, `is`(equalTo(Double.NEGATIVE_INFINITY)))
     }
 
     @Test
     fun subTwoNumbersZero() {
-        val resultAdd = mCalculator.sub(1.0, 0.0)
-        assertThat(resultAdd, `is`(equalTo(1.0)))
+        val resultSub = mCalculator.sub(1.0, 0.0)
+        assertThat(resultSub, `is`(equalTo(1.0)))
     }
 
     @Test
     fun subTwoNumbersLarge() {
-        val resultAdd = mCalculator.sub(10.0.pow(magnitude), 10.0.pow(magnitude))
-        assertThat(resultAdd, `is`(equalTo(0.0)))
+        val resultSub = mCalculator.sub(10.0.pow(magnitude), 10.0.pow(magnitude))
+        assertThat(resultSub, `is`(equalTo(0.0)))
     }
 
-
+    @Test
+    fun divTwoNumbers() {
+        val resultDiv = mCalculator.div(1.0, 2.0)
+        assertThat(resultDiv, `is`(equalTo(0.5)))
+    }
+    @Test(expected = IllegalArgumentException::class)
+    fun divByZero() {
+        val resultDiv = mCalculator.div(1.0, 0.0)
+    }
 }
