185c36a04df2a67e7c65f134565b7a974e9c08ad
Antoine Labard <antoine@labard.fr>
2020-10-15 13:31:20 +0200

    Added more tests for StatisticsUtils.

--- android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
+++ android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
@@ -14,4 +14,35 @@ class StatisticsUtilsKtTest {
         assertEquals(result.completedTasksPercent, 0f)
         assertEquals(result.activeTasksPercent, 100f)
     }
+
+    @Test
+    fun getActiveAndCompletedStats_empty_returnsZeroZero() {
+        val tasks = listOf<Task>()
+        val result = getActiveAndCompletedStats(tasks)
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 0f)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_noActive_returnsZeroHundred() {
+        val tasks = listOf(
+                Task("title", "desc", isCompleted = true)
+        )
+        val result = getActiveAndCompletedStats(tasks)
+        assertEquals(result.completedTasksPercent, 100f)
+        assertEquals(result.activeTasksPercent, 0f)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_both_returnsFiftyFifty() {
+        val tasks = listOf(
+                Task("title", "desc", isCompleted = false),
+                Task("title1", "desc1", isCompleted = true)
+        )
+        val result = getActiveAndCompletedStats(tasks)
+        assertEquals(result.completedTasksPercent, 50f)
+        assertEquals(result.activeTasksPercent, 50f)
+    }
+
+
 }
