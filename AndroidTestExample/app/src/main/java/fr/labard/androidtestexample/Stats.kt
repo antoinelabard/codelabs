c563329efe67f480bc810712b692e4cd5ebb7023
Antoine Labard <antoine@labard.fr>
2020-11-08 21:05:41 +0100

    cleared repository.

--- AndroidTestExample/app/src/main/java/fr/labard/androidtestexample/Stats.kt
+++
@@ -1,8 +0,0 @@
-package fr.labard.androidtestexample
-
-class Stats(private val array: Array<Int>) {
-    fun getMean(): Double {
-        return array.sum() / array.size.toDouble()
-    }
-    fun getSum() = array.sum()
-}
--- RoomDatabaseExample/app/src/main/java/fr/labard/roomdatabaseexample/RecordDatabase.kt
+++
@@ -1,9 +0,0 @@
-package fr.labard.roomdatabaseexample
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [RecordEntity::class], version = 1)
-abstract class RecordDatabase : RoomDatabase() {
-    abstract fun recordDao(): RecordDao
-}
