4bbdc2bc9684baa61d72c58875a646c865dd34d1
Antoine Labard <antoine@labard.fr>
2020-11-08 21:58:11 +0100

    Inital commit in branch.

---
+++ app/src/main/java/fr/labard/livedataexample/RecordViewModel.kt
@@ -0,0 +1,11 @@
+package fr.labard.livedataexample
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RecordViewModel: ViewModel() {
+    val records: MutableLiveData<Int>
+        get() {
+            return records
+        }
+}
